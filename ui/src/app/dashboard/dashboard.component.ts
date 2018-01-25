9f3c9109b12e4fe2d649647eb7ebe37e5c50f291
Priyank Upadhyay <priyank@platinos.in>
2018-01-25 21:08:20 +0530

    bleh

--- ui/src/app/dashboard/dashboard.component.ts
+++ ui/src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import * as Chartist from 'chartist';
 
+
 declare var $:any;
 
 @Component({
@@ -10,95 +11,71 @@ declare var $:any;
 })
 
 export class DashboardComponent implements OnInit{
+  reward = 0; 
+  static tokens: number = 0;
+  static playCount: number=0;
+  constructor() {
+  
+  }
+  playGame(){
+    //alert('bleh');
+    	
+    DashboardComponent.playCount++;
+    
+    $( "#slotGif" ).show();
+    $(".slots").hide();
+    $(".playBtn").hide();
+    setTimeout(function(){
+      console.log("Entered in timeout")
+      var left = Math.floor((Math.random() * 8) + 1);
+      var middle = Math.floor((Math.random() * 8) + 1);
+      var right = Math.floor((Math.random() * 8) + 1);
+      //alert(left+"::"+middle+"::"+right);
+      $('#leftSlot').attr("src","https://dummyimage.com/200x200/f516f5/fff&text="+left);
+      $('#middleSlot').attr("src","https://dummyimage.com/200x200/64f516/fff&text="+middle);
+      $('#rightSlot').attr("src","https://dummyimage.com/200x200/f5a716/fff&text="+right);
+      $( "#slotGif" ).hide();
+      $( ".slots" ).show();
+      $(".playBtn").show();
+      $( "#playBtn" ).text("Play Again");
+      var score = 0;
+      if(left==middle && middle==right){
+        if(left=7){
+          score = 100;
+        }
+        else score = 80;
+      }
+      else if(left==middle || left==right || middle==right){
+        score = 50;
+      }
+      else{
+        score =  Math.floor((Math.random() * 29) + 1);
+      }
+      
+      DashboardComponent.tokens += (score);
+      //alert(this.tokens);
+      
+      $("#rewardNum").html("<p>Rewards</p>$"+score);
+      $("#tokensNum").html("<p>Tokens</p>$"+DashboardComponent.tokens);
+    
+      if(DashboardComponent.playCount==3){
+
+      DashboardComponent.playCount = 0;
+        
+      $(".playBtn").show();
+      $( "#playBtn" ).text("Play Now");
+      DashboardComponent.tokens=0;
+      setTimeout(function(){$( ".slots" ).hide();$("#tokensNum").html("<p>Tokens</p>$"+DashboardComponent.tokens);},3000);
+      }
+    },2500);
+
+  
+    
+  }
     ngOnInit(){
-        var dataSales = {
-          labels: ['9:00AM', '12:00AM', '3:00PM', '6:00PM', '9:00PM', '12:00PM', '3:00AM', '6:00AM'],
-          series: [
-             [287, 385, 490, 562, 594, 626, 698, 895, 952],
-            [67, 152, 193, 240, 387, 435, 535, 642, 744],
-            [23, 113, 67, 108, 190, 239, 307, 410, 410]
-          ]
-        };
-
-        var optionsSales = {
-          low: 0,
-          high: 1000,
-          showArea: true,
-          height: "245px",
-          axisX: {
-            showGrid: false,
-          },
-          lineSmooth: Chartist.Interpolation.simple({
-            divisor: 3
-          }),
-          showLine: true,
-          showPoint: false,
-        };
-
-        var responsiveSales: any[] = [
-          ['screen and (max-width: 640px)', {
-            axisX: {
-              labelInterpolationFnc: function (value) {
-                return value[0];
-              }
-            }
-          }]
-        ];
-
-        new Chartist.Line('#chartHours', dataSales, optionsSales, responsiveSales);
-
-
-        var data = {
-          labels: ['Jan', 'Feb', 'Mar', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-          series: [
-            [542, 543, 520, 680, 653, 753, 326, 434, 568, 610, 756, 895],
-            [230, 293, 380, 480, 503, 553, 600, 664, 698, 710, 736, 795]
-          ]
-        };
-
-        var options = {
-            seriesBarDistance: 10,
-            axisX: {
-                showGrid: false
-            },
-            height: "245px"
-        };
-
-        var responsiveOptions: any[] = [
-          ['screen and (max-width: 640px)', {
-            seriesBarDistance: 5,
-            axisX: {
-              labelInterpolationFnc: function (value) {
-                return value[0];
-              }
-            }
-          }]
-        ];
-
-        new Chartist.Line('#chartActivity', data, options, responsiveOptions);
-
-        var dataPreferences = {
-            series: [
-                [25, 30, 20, 25]
-            ]
-        };
-
-        var optionsPreferences = {
-            donut: true,
-            donutWidth: 40,
-            startAngle: 0,
-            total: 100,
-            showLabel: false,
-            axisX: {
-                showGrid: false
-            }
-        };
-
-        new Chartist.Pie('#chartPreferences', dataPreferences, optionsPreferences);
-
-        new Chartist.Pie('#chartPreferences', {
-          labels: ['62%','32%','6%'],
-          series: [62, 32, 6]
-        });
+      $("#slotGif").hide();
+      $(".slots").hide();
+      $("#rewardNum").html("<p>Rewards</p>$"+this.reward);
+      //alert(this.tokens);
     }
 }
--- ui/src/app/table/table.component.ts
+++ ui/src/app/table/table.component.ts
@@ -18,12 +18,12 @@ export class TableComponent implements OnInit{
         this.tableData1 = {
             headerRow: [ 'Id', 'Name', 'Rewards'],
             dataRows: [
-                ['1', 'Bleh Sharma','$36,738'],
-                ['1', 'Bleh Sharma','$36,738'],
-                ['1', 'Bleh Sharma','$36,738'],
-                ['1', 'Bleh Sharma','$36,738'],
-                ['1', 'Bleh Sharma','$36,738'],
-                ['1', 'Bleh Sharma','$36,738']
+                ['1', 'Anshul Sharma','$36,738'],
+                ['2', 'Seema Kad','$30,534'],
+                ['3', 'Sneha Joshi','$28,766'],
+                ['4', 'Soumya Arora','$26,923'],
+                ['5', 'Snigdha Tamot','$20,666'],
+                ['6', 'Priya Shetty','$16,222']
             ]
         };
     }
